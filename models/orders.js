b9846241ea72813982d34e5582de997c37f794c6
Nabeel <nabeelajmeri0@gmail.com>
2026-09-08 22:08:02 +0500

    almost done

--- models/orders.js
+++ models/orders.js
@@ -1,3 +1,4 @@
+
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
@@ -30,6 +31,24 @@ const orderSchema = new mongoose.Schema(
             },
         ],
 
+        subtotal: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
+        discount: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
+        deliveryFee: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
         totalPrice: {
             type: Number,
             required: true,
@@ -54,3 +73,62 @@ const orderSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Order", orderSchema);
+
+
+
+// const mongoose = require("mongoose");
+
+// const orderSchema = new mongoose.Schema(
+//     {
+//         user: {
+//             type: mongoose.Schema.Types.ObjectId,
+//             ref: "User",
+//             required: true,
+//         },
+
+//         items: [
+//             {
+//                 product: {
+//                     type: mongoose.Schema.Types.ObjectId,
+//                     ref: "Product",
+//                     required: true,
+//                 },
+
+//                 quantity: {
+//                     type: Number,
+//                     required: true,
+//                     min: 1,
+//                 },
+
+//                 price: {
+//                     type: Number,
+//                     required: true,
+//                     min: 0,
+//                 },
+//             },
+//         ],
+
+//         totalPrice: {
+//             type: Number,
+//             required: true,
+//             min: 0,
+//         },
+
+//         status: {
+//             type: String,
+//             enum: [
+//                 "pending",
+//                 "processing",
+//                 "shipped",
+//                 "delivered",
+//                 "cancelled",
+//             ],
+//             default: "pending",
+//         },
+//     },
+//     {
+//         timestamps: true,
+//     }
+// );
+
+// module.exports = mongoose.model("Order", orderSchema);
